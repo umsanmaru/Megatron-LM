799751e18c4b5bb344becdce904d4416825ecfd6
umsammaru <um3maru@kaist.ac.kr>
2025-03-11 00:24:13 +0900

    change to 2 nodes x 1 gpu

--- examples/pretrain_gpt_distributed.sh
+++ examples/pretrain_gpt_distributed.sh
@@ -6,7 +6,7 @@ export CUDA_DEVICE_MAX_CONNECTIONS=1
 export NCCL_SOCKET_IFNAME=ens3
 export GLOO_SOCKER_IFNAME=ens3
 
-GPUS_PER_NODE=4 # testing 241205
+GPUS_PER_NODE=1 # testing 241205
  
 # #single node
 # MASTER_ADDR=localhost
@@ -56,7 +56,7 @@ GPT_ARGS="
     --lr-warmup-fraction .01 \
     --clip-grad 1.0 \
     --use-mcore-models \
-    --num-experts 8 \
+    --num-experts 2 \
     --expert-model-parallel-size 1 \
     --use-distributed-optimizer \
 "
